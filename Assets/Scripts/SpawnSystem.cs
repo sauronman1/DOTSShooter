7d357385b519963e557deb95bc983ca9f4a79682
sauronman1 <aboud-malki@hotmail.com>
2021-10-21 10:33:22 +0200

    removed comments

--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -14,11 +14,8 @@ public class SpawnSystem : SystemBase
     {
         double time = Time.ElapsedTime;
         
-        // There is a single Settings component in the scene.
         var settings = GetSingleton<Settings>();
         var randomArray = World.GetExistingSystem<RandomSystem>().RandomArray;
-        // var instances = new NativeArray<Entity>(settings.Count, Allocator.Temp);
-        //EntityManager.Instantiate(settings.Prefab, instances);
 
         if (time > delay && entityIndex < settings.count)
         {
@@ -28,11 +25,6 @@ public class SpawnSystem : SystemBase
             entityIndex++;
             delay += 2;
         }
-
         
-        // Native containers require explicit cleanup. There are special rules around
-        // Allocator.Temp that do not require cleanup, but calling Dispose is never wrong.
-
-        // Disabling the system so it only runs once, this one is for initialization only.
     }
 }
